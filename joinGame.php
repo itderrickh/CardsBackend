7053817b2aed127aa1c82db3dd40f933d3bb8248
Derrick Heinemann <itderrickh@gmail.com>
2016-11-28 13:11:26 -0600

    Added an endpoint to join game

--- joinGame.php
+++ joinGame.php
@@ -2,33 +2,25 @@
 require_once './config.php';
 require_once './dbclasses/user.php';
 require_once './dbclasses/game.php';
-require_once './dbclasses/card.php';
 require_once './verify.php';
 $AJAX_FORM = json_decode(file_get_contents('php://input'), true);
 
 $userDao = new UserDAO($config);
 $gameDao = new GameDAO($config);
-$cardDao = new CardDAO($config);
 
 $result = array();
 $result['success'] = false;
 $token = $_SERVER['HTTP_AUTHORIZE'];
 
 if(verifyToken($token, $config)) {
-    $gameId = $gameDao->getOrCreateGame();
+    $tokenInfo = getTokenInfo($token);
+    $user = $userDAO->getUser($tokenInfo['email']);
+    $gameId = $gameDao->getCurrentGame();
 
-    if($gameDao->isSetup($gameId)) {
+    if(!$gameDao->isGameReady($gameId)) {
+        $gameDao->addUserToGame($user['id'], $gameId);
         $result['success'] = true;
-    } else {
-        if($gameDao->isGameReady($gameId)) {
-            $deck = $cardDao->getCards();
-            $users = $userDao->getGameUsers($gameId);
-
-            $gameDao->startGame($gameId, $deck, $users);
-            $result['success'] = true;
-        }
     }
-    
 }
 
 echo json_encode($result);
